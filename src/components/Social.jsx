295b66c634c51540bc535c724b1a7bea988d7040
Mathew Samir <85489453+Mathew-Samir@users.noreply.github.com>
2024-10-07 17:32:51 +0300

    Update Social.jsx

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -13,13 +13,24 @@ return (
                         <FontAwesomeIcon icon={faMapMarkerAlt} className="text-red-500 " />
                         <p className='inline-block px-2 font-bold'>Location: El_Zamalek </p>
                     </div>
-                    <div className="p-1 bg-lime-500 w-[75%] lg:w-[140%] mt-2 rounded-md mx-11 lg:mx-0">
-                        <a href="https://maps.app.goo.gl/Fwu5BppgP5iA5FN2A">
+                    <div className="p-1 bg-lime-500 w-[77%] lg:w-[150%] lg:h-[16em]  mt-2 rounded-md mx-11 lg:mx-0">
+                        <iframe 
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27626.
+                            696971608086!2d31.2217712!3d30.05586975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.
+                            1!3m3!1m2!1s0x145840e059d98225%3A0x91cb6f582e8b215c!2sZamalek%2C%20Cairo%
+                            20Governorate!5e0!3m2!1sen!2seg!4v1728309690412!5m2!1sen!2seg" 
+                            width="100%" 
+                            height="100%" 
+                            style={{ border: 0 }}  // Fixed style prop
+                            allowFullScreen=""  // Fixed camelCase prop
+                            loading="lazy">
+                        </iframe>
+                        {/* <a href="https://maps.app.goo.gl/Fwu5BppgP5iA5FN2A">
                             <img src={'https://discoverdiscomfort.com/wp-content/uploads/2018/12/Best-Neighbourhoods-in-Cairo-Zamalek-Gezira-768x520.jpg'}
                             className="w-full h-40 md:h-30 object-cover rounded-md"
                             alt='location'
-                        />
-                        </a>    
+                            />
+                        </a>     */}
                     </div>
                 </div>
                 <div className="md:mt-12 mt-4 mb-8 flex items-center justify-center">
